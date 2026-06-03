95769f1be16d48030553cde0184b8f72b62e1dc0
Mariyan Karakiev <77015673+MariyanKarakiev@users.noreply.github.com>
2026-06-03 18:45:38 +0300

    Refactor astro.config.mjs for Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+  import tailwindcss from '@tailwindcss/vite';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://m-karakiev.github.io/kart13-website/',
-  base: '/kart13-website',
-  i18n: {
-    defaultLocale: 'bg',
-    locales: ['bg', 'en', 'de', 'fr', 'nl'],
-    routing: {
-      prefixDefaultLocale: true,
+  export default defineConfig({
+    site: 'https://m-karakiev.github.io/kart13-website/',
+    base: '/kart13-website',
+    i18n: {
+      defaultLocale: 'bg',
+      locales: ['bg', 'en', 'de', 'fr', 'nl'],
+      routing: {
+        prefixDefaultLocale: true,
+      },
     },
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    vite: {
+      plugins: [tailwindcss()],
+      ssr: {
+        external: ['aos']
+      }
+    }
+  });
