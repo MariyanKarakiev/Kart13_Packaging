0c7530bad0e4f65d1a46cdb4fb636672da70a050
Mariyan Karakiev <77015673+MariyanKarakiev@users.noreply.github.com>
2026-06-04 11:35:09 +0300

    Remove site and base properties from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
   import tailwindcss from '@tailwindcss/vite';
 
   export default defineConfig({
-    site: 'https://m-karakiev.github.io/kart13-website/',
-    base: '/kart13-website',
     i18n: {
       defaultLocale: 'bg',
       locales: ['bg', 'en', 'de', 'fr', 'nl'],
